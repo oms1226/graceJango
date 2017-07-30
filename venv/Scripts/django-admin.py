a9dd20c4522ae97caa00627b521c786613e644ee
oms1226 <oms1226@gmail.com>
2017-07-31 08:11:03 +0900

    venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\oms12\AppData\Local\lxss\home\oms1226\workspaces\python\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
